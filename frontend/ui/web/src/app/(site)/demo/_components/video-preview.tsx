7af11dba3862180f1c8913fe68d55ab739a1f6ef
AlbertZhaoCA <zhaoq@kean.edu>
2025-02-12 22:33:30 +0800

    feat: 完成 demo 页原型 - 提供视频预览 - 响应式提供以 stream 为回复的 llm 对话框 - @todos: 重构，将组件以components的形式提供，便于维护。依据后续需求完善对话框

--- frontend/ui/web/src/app/(site)/demo/_components/video-preview.tsx
+++ frontend/ui/web/src/app/(site)/demo/_components/video-preview.tsx
@@ -7,7 +7,10 @@ export default function VideoPreview() {
   } = useContext(DemoContext)!;
   return (
     <>
-      <video className="w-[400px] sm:w-[600px] lg:w-[800px]" controls>
+      <video
+        className="w-[400px] sm:w-[600px] lg:w-[800px] rounded-2xl bg-black dark:bg-white shadow-2xl"
+        controls
+      >
         <source src={URL.createObjectURL(videoFile!)} type={videoFile!.type} />
         Your browser does not support the video tag.
       </video>
